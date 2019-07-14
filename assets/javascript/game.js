a97c80a28043438edc6406f332480b90e55274b7
John Michael Pintor <jpintor1018@yahoo.com>
2019-07-14 19:05:55 -0400

    CSS-Update

---
+++ assets/javascript/game.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+
+// HEROES
+
+var Yoda = {
+    name :"Yoda",
+    health : 100,
+    attack : 12,
+    image : '<img src="assets/images/yoda.png"></img>'
+}
+var Han = {
+    name :"Han Solo",
+    health : 85,
+    attack: 4,
+    image: '<img src="assets/images/han-solo.png"></img>'
+}
+var Chewie = {
+    name :"Chewbacca",
+    health : 95,
+    attack: 8,
+    image: '<img src="assets/images/han-solo.png"></img>'
+}
+var Obi = {
+    name :"Obi-Wan",
+    health : 100,
+    attack: 13,
+    image: '<img src="assets/images/han-solo.png"></img>'
+}
+
+// VILLAINS
+var Maul = {
+    name :"Darth Maul",
+    health : 110,
+    attack: 10,
+    image: '<img src="assets/images/han-solo.png"></img>'
+}
+var Vader = {
+    name :"Darth Vader",
+    health : 125,
+    attack: 12,
+    image: '<img src="assets/images/han-solo.png"></img>'
+}
+var Pal = {
+    name :"General Palpatine",
+    health : 180,
+    attack: 15,
+    image: '<img src="assets/images/han-solo.png"></img>'
+}
+var Gri = {
+    name :"General Grievous",
+    health : 105,
+    attack: 7,
+    image: '<img src="assets/images/han-solo.png"></img>'
+}
+
+
+var char = [Yoda, Han, Chewie, Obi, Maul, Vader, Pal, Gri]
+
+
+for( i=0; i<char.length;i++){
+    $(".img-thumbnail").click(function(){
+        if( attack = 15)
+    {
+        console.log("fjdklsajfkldsa")
+    }
+    })
+}
+
+});
